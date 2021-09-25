fea635a5666a303a74e82ac9a657df2a02cf3d2d
Daniel Carazo <danielcarazo20@gmail.com>
2021-09-25 00:27:47 -0400

    completed idb file

---
+++ public/js/idb.js
@@ -0,0 +1,51 @@
+let db;
+
+const request = indexedDB.open('tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result; // save ref to db
+    db.createObjectStore('budget', { autoIncrement: tr }); // create object store table called budget 
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if(navigator.onLine) 
+    uploadData();
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('budget');
+    
+    budgetObjectStore.add(record);
+};
+
+function uploadData() {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('budget');
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = () => {
+        fetch('/api/transaction/bulk', {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: 'application/json, text/plain', 
+                'Content-type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            const transaction = db.transaction(['budget'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('budget');
+            budgetObjectStore.clear();
+        })
+        .catch(err => {console.log(err)})
+    }
+};
+
+window.addEventListener('online', uploadData);
